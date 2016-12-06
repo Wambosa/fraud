047653af4255cf8d86a90d46a372246f1fbe00a2
Shon Diaz <shondiaz@gmail.com>
2016-12-06 15:39:24 +0000

    refactor to imporve readability and code intent

--- index.js
+++ index.js
@@ -11,33 +11,33 @@ const fontPath = `${root}/fonts/LibreBaskerville-Regular.ttf`;
 
 var gen = null;
 
-if(!fs.existsSync(buildDir))
-	fs.mkdirSync(buildDir);
-
-cache.preloadDocuments(function(err) {
-	if(err) throw err;
-	console.log(`optimal pre-load  of ${cache.docs.length} documents`);
-	main(process.argv.splice(2));
-});
+main(process.argv.splice(2));
 
 function main(args) {
 
-    let fileCount = +args[0] || 1;
-    let dataGen = args[1] || './example/exampleGenerator.js';
+    if(!fs.existsSync(buildDir))
+    	fs.mkdirSync(buildDir);
 
-    console.log(`BEGIN Image Generation of ${fileCount} ish files using ${dataGen} generator`);
-
-    gen = require(dataGen);
-    if(!gen)
-        throw `problem loading data generator: ${dataGen}`;
-
-    recurseCreate(fileCount)
-        .then(function(){
-
+    cache.preloadDocuments().then(function() {
+    	    
+    	console.log(`optimal pre-load  of ${cache.docs.length} documents`);
+    
+        let fileCount = +args[0] || 1;
+        let dataGen = args[1] || './example/exampleGenerator.js';
+    
+        console.log(`BEGIN Image Generation of ${fileCount} ish files using ${dataGen} generator`);
+    
+        gen = require(dataGen);
+        if(!gen)
+            return Promise.reject(`problem loading data generator: ${dataGen}`);
+    
+        return recurseCreate(fileCount).then(function(){
             cache.destroy();
             console.log("done!");
         })
-        .catch(console.error);
+        .catch(console.error); //erase this soon
+    })
+    .catch(console.error);
 }
 
 function recurseCreate(countDown) {
@@ -54,57 +54,58 @@ function recurseCreate(countDown) {
         cache.docs.forEach(function(doc) {
             let img = doc.gdImg;
 
-            gd.create(img.width, img.height, function(err, dest) {
+            gd.create(img.width, img.height, function(err, blankImage) {
+                if(err)
+                    return reject(err);
 
-                img.copy(dest, 0, 0, 0, 0, dest.width, dest.height);
+                img.copy(blankImage, 0, 0, 0, 0, blankImage.width, blankImage.height);
 
 	            let saveName = `${person.first}_${person.last}_${doc.class}`;
-
-	            let rotations = {
-		            up: {type: "vertical", angle: 0, width: img.width, height: img.height},
-		            left: {angle: 90, width: img.height, height: img.width},
-		            down: {type: "vertical", angle: 180, width: img.width, height: img.height},
-		            right: {angle: 270, width: img.height, height: img.width}
-	            };
-
+	            
 	            let appliedRotation = doc.rotation || ["up", "left", "down", "right"][Math.floor(Math.random()*4)];
-	            let r = rotations[appliedRotation];
 
 	            let myPromiseToYou = Promise.resolve();
 
                 if(doc.fields)
-	                myPromiseToYou = writeTextOnImage(doc, person, dest).then(function(boundingBoxes){
-			                return saveMetadata({
-				                name: saveName,
-				                class: doc.class,
-				                rotation: appliedRotation,
-				                fields: boundingBoxes.map(function(box){
-					                return rotateBoundingBox(box, {width: r.width, height: r.height}, appliedRotation);
-				                })
-			                });
+                    myPromiseToYou = writeTextOnImage(doc, person, blankImage).then(function(boundingBoxes){
+                        return saveMetadata({
+                            name: saveName,
+                            class: doc.class,
+                            rotation: appliedRotation,
+                            fields: boundingBoxes.map(function(box){
+                                return {
+                                    name: box.name,
+                                    bounds: rotateBoundingBox(box.bounds, {width: r.width, height: r.height}, appliedRotation)
+                                };
+                            })
+                        });
 	                });
+
+	            let r = getRotationConfig(img, appliedRotation);
 
 	            myPromiseToYou.then(function() {
 		            gd.create(r.width, r.height, function(err, finalImage){
+                        if(err)
+                            return reject(err);
 
 			            if(r.type == "vertical")
-				            dest.copyRotated(finalImage, r.width*.5, r.height*.5, 0, 0, r.width, r.height, r.angle);
+				            blankImage.copyRotated(finalImage, r.width*.5, r.height*.5, 0, 0, r.width, r.height, r.angle);
 			            else
-				            dest.copyRotated(finalImage, r.width*.5, r.height*.5, 0, 0, r.height, r.width, r.angle);
+				            blankImage.copyRotated(finalImage, r.width*.5, r.height*.5, 0, 0, r.height, r.width, r.angle);
 
-			            dest.destroy();
+                        blankImage.destroy();
 
 			            finalImage.saveJpeg(`build/${saveName}.jpg`, 75, function(err) {
 				            if(err)
-					            reject(err);
+                                return reject(err);
 
-				            console.log(`saved: ${saveName}`);
+                            console.log(`saved: ${saveName}`);
 
 				            finalImage.destroy();
 
 				            if(++savedFiles === cache.docs.length) {
-					            console.log(`${(countDown - cache.docs.length)} files left to go`);
-					            resolve(recurseCreate(countDown - cache.docs.length));
+                                console.log(`${(countDown - cache.docs.length)} files left to go`);
+                                resolve(recurseCreate(countDown - cache.docs.length));
 				            }
 			            });
 		            });
@@ -113,6 +114,15 @@ function recurseCreate(countDown) {
             });
         });
     });
+}
+
+function getRotationConfig(img, appliedRotation){
+    return {
+        up: {type: "vertical", angle: 0, width: img.width, height: img.height},
+        left: {angle: 90, width: img.height, height: img.width},
+        down: {type: "vertical", angle: 180, width: img.width, height: img.height},
+        right: {angle: 270, width: img.height, height: img.width}
+    }[appliedRotation];
 }
 
 function saveMetadata(object){
@@ -134,7 +144,7 @@ function writeTextOnImage(doc, person, dest){
 	let sizeDeviation = person.handwriting && person.handwriting.size || 1;
 	let angle = person.handwriting && person.handwriting.angle;
 
-	let col = textColor || dest.colorAllocate(255,0,0,1);
+	let col = textColor || dest.colorAllocate(0,0,0,1);
 
 	doc.fields.forEach(function(field) {
 
@@ -166,18 +176,30 @@ function writeTextOnImage(doc, person, dest){
 	return Promise.all(iPromiseYou);
 }
 
-function rotateBoundingBox(box, img, appliedRotation){
-	//we only want the top-left XY then bottom-right XY
-	//note: this is relative. would be better if it perfectly represented the after rotated version
-	let adjusted = {
-		"up": [box.bounds[6], box.bounds[7], box.bounds[2], box.bounds[3]],
-		"left": [box.bounds[7], img.height - box.bounds[6], box.bounds[3], img.height - box.bounds[2]],
-		"down": [img.width - box.bounds[6], img.height - box.bounds[7], img.width - box.bounds[2], img.height - box.bounds[3]],
-		"right": [img.width - box.bounds[7], box.bounds[6], img.width - box.bounds[3], box.bounds[2]]
-	}[appliedRotation];
 
+/*
+box.bounds = [xll, yll, xlr, ylr, xur, yur, xul, yul]
+
+   xul           xur
+yul ._____________. yur
+    | Lorem ipsum |
+    | dolor sit   |
+yll ._____________. ylr
+   xll           xlr
+   
+legend:
+xul = x upper left
+ylr = y lower right
+
+i only want [xul, yul, xlr, ylr]
+
+returns rotated bounds; relative to the new upper left corner, which could be a problem in some cases
+*/
+function rotateBoundingBox(bounds, img, appliedRotation){
 	return {
-		name: box.name,
-		bounds: adjusted
-	};
+		"up": [bounds[6], bounds[7], bounds[2], bounds[3]],
+		"left": [bounds[7], img.height - bounds[6], bounds[3], img.height - bounds[2]],
+		"down": [img.width - bounds[6], img.height - bounds[7], img.width - bounds[2], img.height - bounds[3]],
+		"right": [img.width - bounds[7], bounds[6], img.width - bounds[3], bounds[2]]
+	}[appliedRotation];
 }
--- src/DocumentCache.js
+++ src/DocumentCache.js
@@ -14,30 +14,27 @@ module.exports = {
         });
     },
 
-    preloadDocuments: function(callback){
+    preloadDocuments: function(){
         let gd = require('node-gd');
 
         let count = 0;
-
-        templateMetadata.docs.forEach(function(doc){
-
-            if(doc.form && doc.form.length > 35)
-                console.warn(`\n\n\nHEY! doc ${doc.class} has ${doc.form.length} fields`,
-                    'node-gd tends to silent fail some text rendering with too many fields or characters',
-                    'roughly greater than 425 characters can get the bug.\n\n\n');
-
-            gd.openJpeg(`./data/${doc.file}`, function(err, img) {
-                if (err)
-                    callback(err);
-
-                if(!doc.isColor)
-                    img.grayscale();
-
-                doc.gdImg = img;
-                docsPreload.push(doc);
-
-                if(++count === templateMetadata.docs.length)
-                    callback();
+        
+        return new Promise(function(resolve, reject){
+            templateMetadata.docs.forEach(function(doc){
+    
+                gd.openJpeg(`./data/${doc.file}`, function(err, img) {
+                    if(err)
+                        return reject(err);
+    
+                    if(!doc.isColor)
+                        img.grayscale();
+    
+                    doc.gdImg = img;
+                    docsPreload.push(doc);
+    
+                    if(++count === templateMetadata.docs.length)
+                        resolve();
+                });
             });
         });
     }
